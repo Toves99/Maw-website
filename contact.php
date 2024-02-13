922c4e6534e038246ec63777b3a8f2000dc4c75c
Toves99 <clintonekesa90@gmail.com>
2024-02-13 22:11:52 +0300

    version

---
+++ contact.php
@@ -0,0 +1,292 @@
+
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>maw institute</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Raleway:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/contact2.css" rel="stylesheet">
+
+  <style>
+    #btt{
+      background: white;
+      color: black;
+      border-radius: 5px;
+      margin-left: 30px;
+      border:1px solid blue;
+      height: 40px;
+      width:130px;
+    }
+     
+    @media (max-width: 768px) {
+   
+    #btt{
+      background: white;
+      color: black;
+      border-radius: 5px;
+      margin-left: 0;
+      border:1px solid blue;
+      height: 30px;
+      width:100px;
+    }
+  }
+  </style>
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <section id="topbar" class="topbar d-flex align-items-center">
+    <div class="container d-flex justify-content-center justify-content-md-between">
+      <div class="contact-info d-flex align-items-center">
+        <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:contact@example.com">info@mawinstitute.com</a></i>
+        <i class="bi bi-phone d-flex align-items-center ms-4"><span>+254715744358</span></i>
+      </div>
+      <div class="social-links d-none d-md-flex align-items-center">
+        <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+        <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></i></a>
+      </div>
+    </div>
+  </section><!-- End Top Bar -->
+
+  <header id="header" class="header d-flex align-items-center">
+
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+      <a href="index.php" class="logo d-flex align-items-center">
+        <!-- Uncomment the line below if you also wish to use an image logo -->
+        <!-- <img src="assets/img/logo.png" alt=""> -->
+        <h1>Maw Institute <br>
+          <span style="font-size:12px;color:#f85a40;margin-left:-5px;">Of Digital Engineering</span>
+        </h1>
+      </a>
+      <nav id="navbar" class="navbar">
+        <ul>
+        <li class="dropdown"><a href="#"><span>Our Courses</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+            <ul>
+              <li><a href="http://157.230.37.96/maw/">All Courses</a></li>
+              <li><a href="http://157.230.37.96/maw/indexAndroid.php">Mobile app development courses</a></li>
+              <li><a href="http://157.230.37.96/maw/indexWeb.php">Web Application develoment courses</a></li>
+              <li><a href="http://157.230.37.96/maw/indexDatabase.php">Database programming courses</a></li>
+              <li><a href="http://157.230.37.96/maw/indexShortCourses.php">Short Courses </a>
+                
+              </li>
+            </ul>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="faq.php">FAQs</a></li>
+          </li>
+          <a href="http://157.230.37.96/maw/">
+            <button id="btt"><i class="bi bi-box-arrow-in-right" style="font-size:18px;font-style:normal;"> login</i></button>
+          </a>
+        </ul>
+      </nav><!-- .navbar -->
+
+      <i class="mobile-nav-toggle mobile-nav-show bi bi-list" style="color:black;"></i>
+      <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x" style="color:black;"></i>
+
+    </div>
+  </header><!-- End Header -->
+  <!-- End Header -->
+
+ 
+
+  <man id="main">
+  <!-- ======= Contact Section ======= -->
+  <section id="contact" class="contact">
+      <div class="container" data-aos="fade-up" data-aos-delay="100">
+
+        <div class="row gy-4">
+          <div class="col-lg-6">
+            <div class="info-item  d-flex flex-column justify-content-center align-items-center">
+              <i class="bi bi-map" style="color:blue;"></i>
+              <h3>Our Address</h3>
+              <p>Utali house, Nairobi, Kenya</p>
+            </div>
+          </div><!-- End Info Item -->
+
+          <div class="col-lg-3 col-md-6">
+            <div class="info-item d-flex flex-column justify-content-center align-items-center">
+              <i class="bi bi-envelope" style="color:blue;"></i>
+              <h3>Email Us</h3>
+              <p>info@mawinstitute.com</p>
+            </div>
+          </div><!-- End Info Item -->
+
+          <div class="col-lg-3 col-md-6">
+            <div class="info-item  d-flex flex-column justify-content-center align-items-center">
+              <i class="bi bi-telephone" style="color:blue;"></i>
+              <h3>Call Us</h3>
+              <p>+254715744358</p>
+            </div>
+          </div><!-- End Info Item -->
+
+        </div>
+
+        <div class="row gy-4 mt-1">
+
+          <div class="col-lg-6 ">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.818169767559!2d36.81373007382504!3d-1.282929635620063!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f10d22f42bf35%3A0x449d7ec7b378dfeb!2sUtalii%20House!5e0!3m2!1sen!2ske!4v1688805687080!5m2!1sen!2ske" frameborder="0" style="border:0; width: 100%; height: 384px;" allowfullscreen></iframe>
+          </div><!-- End Google Maps -->
+
+          <div class="col-lg-6">
+          <h4 class="sent-notification"></h4>
+            <form id="myForm" class="php-email-form">
+              <div class="row gy-4">
+                <div class="col-lg-6 form-group">
+                  <input type="text" id="name" class="form-control" id="name" placeholder="Your Name" required>
+                </div>
+                <div class="col-lg-6 form-group">
+                  <input type="email" class="form-control" id="email" id="email" placeholder="Your Email" required>
+                </div>
+              </div>
+              <div class="form-group">
+                <input type="text" class="form-control" id="subject" id="subject" placeholder="Subject" required>
+              </div>
+              <div class="form-group">
+                <textarea class="form-control" id="message" rows="5" placeholder="Message" required></textarea>
+              </div>
+              <div class="text-center"><button type="submit" onclick="sendEmail()" style="background-color:blue;">Send Message</button></div>
+            </form>
+          </div><!-- End Contact Form -->
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer" class="footer">
+
+    <div class="container">
+      <div class="row gy-4">
+        <div class="col-lg-5 col-md-12 footer-info">
+          <a href="index.html" class="logo d-flex align-items-center">
+            <span>Maw Institute</span>
+          </a>
+          <p>Unlock your software development and software engineering potential at Maw institute.Here, we transform aspiring software engineers and software developers into industry-ready professionals</p>
+          <div class="social-links d-flex mt-4">
+            <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+            <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+            <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+            <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+          </div>
+        </div>
+
+        <div class="col-lg-2 col-6 footer-links">
+          <h4>Useful Links</h4>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="termsAndCondition.php">Terms of service</a></li>
+            <li><a href="privacy.php">Privacy policy</a></li>
+          </ul>
+        </div>
+
+        <div class="col-lg-2 col-6 footer-links">
+          <h4>Our Courses</h4>
+          <ul>
+          <li><a href="http://157.230.37.96/maw/indexShortCourses.php">Short Programming Courses</a></li>
+            <li><a href="http://157.230.37.96/maw/indexWeb.php">Web Development</a></li>
+            <li><a href="http://157.230.37.96/maw/indexAndroid.php">Mobile  App Development</a></li>
+            <li><a href="http://157.230.37.96/maw/indexAndroid.php">Hybrid App Development</a></li>
+            <li><a href="http://157.230.37.96/maw/indexDatabase.php">Database Programming</a></li>
+          </ul>
+        </div>
+
+        <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
+          <h4>Contact Us</h4>
+          <p>
+            Nairobi <br>
+            Utali house<br>
+            Kenya <br><br>
+            <strong>Phone:</strong> +254715744358,+254726586630<br>
+            <strong>Email:</strong>info@mawinstitute.com,<br>admission@mawinstitute.com,<br>sales@mawinstitute.com<br>
+          </p>
+
+        </div>
+
+      </div>
+    </div>
+
+    <div class="container mt-4">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Maw</span></strong>. All Rights Reserved
+      </div>
+      
+    </div>
+
+  </footer><!-- End Footer -->
+  <!-- End Footer -->
+
+  <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+  <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+  <script>
+    function sendEmail(){
+      var name=$("#name");
+      var email=$("#email");
+      var subject=$("#subject");
+      var message=$("#message");
+
+      if(isNotEmpty(name) && isNotEmpty(email) && isNotEmpty(subject) && isNotEmpty(message)){
+        $.ajax({
+          url:'sendEmail.php',
+          method:'POST',
+          dataType:'json',
+          data:{
+            name:name.val(),
+            email:email.val(),
+            subject:subject.val(),
+            message:message.val()
+          }, success:function(response){
+            $("#myForm")[0].reset();
+            $('.sent-notification').text("message sent successfully.");
+          }
+        });
+      }
+    }
+    function isNotEmpty(caller){
+      if(caller.val()==""){
+        caller.css('border','1px solid red');
+        return false;
+      }
+      else{
+        caller.css('border','');
+        return true;
+      }
+    }
+  </script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,304 @@
+
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>maw institute</title>
+  <meta content="" name="description">
+
+  <meta content="" name="keywords">
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Raleway:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/main2.css" rel="stylesheet">
+  <style>
+    #btt{
+      background: white;
+      color: black;
+      border-radius: 5px;
+      margin-left: 30px;
+      border:1px solid blue;
+      height: 40px;
+      width:130px;
+    }
+     #img3{
+      margin-top:-140px;
+      width:180px;
+      height:100px;
+      margin-left:-90px;
+    }
+    @media (max-width: 768px) {
+    #img3{
+      margin-top:-100px;
+      width:180px;
+      height:90px;
+      margin-left:-90px;
+    }
+    #btt{
+      background: white;
+      color: black;
+      border-radius: 5px;
+      margin-left: 0;
+      border:1px solid blue;
+      height: 30px;
+      width:100px;
+    }
+  }
+  </style>
+  
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <section id="topbar" class="topbar d-flex align-items-center">
+    <div class="container d-flex justify-content-center justify-content-md-between">
+      <div class="contact-info d-flex align-items-center">
+        <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:contact@example.com">info@mawinstitute.com</a></i>
+        <i class="bi bi-phone d-flex align-items-center ms-4"><span>+254715744358</span></i>
+      </div>
+      <div class="social-links d-none d-md-flex align-items-center">
+        <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+        <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></i></a>
+      </div>
+    </div>
+  </section><!-- End Top Bar -->
+
+  <header id="header" class="header d-flex align-items-center">
+
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+      <a href="index.php" class="logo d-flex align-items-center">
+        <!-- Uncomment the line below if you also wish to use an image logo -->
+        <!-- <img src="assets/img/logo.png" alt=""> -->
+        <h1>Maw Institute <br>
+          <span style="font-size:12px;color:#f85a40;margin-left:-5px;">Of Digital Engineering</span>
+        </h1>
+      </a>
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li class="dropdown"><a href="#"><span>Courses for You</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+            <ul>
+              <li><a href="http://157.230.37.96/maw/">All Courses</a></li>
+              <li><a href="http://157.230.37.96/maw/indexAndroid.php">Mobile app development courses</a></li>
+              <li><a href="http://157.230.37.96/maw/indexWeb.php">Web Application develoment courses</a></li>
+              <li><a href="http://157.230.37.96/maw/indexDatabase.php">Database programming courses</a></li>
+              <li><a href="http://157.230.37.96/maw/indexShortCourses.php">Short Courses </a>
+              </li>
+            </ul>
+          </li>
+          <li><a href="contact.php">Contact</a></li>
+          <li><a href="faq.php">FAQs</a></li>
+
+          <a href="http://157.230.37.96/maw/">
+            <button id="btt"><i class="bi bi-box-arrow-in-right" style="font-size:18px;font-style:normal;"> login</i></button>
+          </a>
+        </ul>
+      </nav><!-- .navbar -->
+
+      <i class="mobile-nav-toggle mobile-nav-show bi bi-list" style="color:black;"></i>
+      <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x" style="color:black;"></i>
+
+    </div>
+  </header><!-- End Header -->
+  <!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="hero">
+    <div class="container position-relative">
+      <div class="row gy-5" data-aos="fade-in">
+        <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center text-center text-lg-start">
+          <h5>Ready to grow your career in software development and software engineering</h5>
+          <p>Unlock your software development and software engineering potential at Maw institute.Here, we transform aspiring software engineers and software developers into industry-ready professionals</p>
+          <br>
+          <div class="d-flex justify-content-center justify-content-lg-start">
+            <a href="http://157.230.37.96/maw/SignUp.php" >
+              <button class="btn4" >Apply Now</button>
+            </a>
+            <!--<a href="" class="glightbox btn-watch-video d-flex align-items-center"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>-->
+          </div>
+          <div class="col-lg-6 order-1 order-lg-2">
+            <img src="assets/img/code.jpg"  alt="" data-aos="zoom-out" data-aos-delay="100" class="img1">
+          </div>
+        </div>
+        <div class="col-lg-6 order-1 order-lg-2">
+          
+        </div>
+      </div>
+    </div>
+    <div class="icon-boxes position-relative">
+      <div class="container position-relative">
+        <div class="row gy-4 mt-5">
+
+          <div class="col-xl-3 col-md-6" data-aos="fade-up" data-aos-delay="100">
+            <div class="icon-box">
+              <img src="assets/img/mob.png" id="img3">
+              <h4>Start your Mobile App development Career with Us</h4>
+              <br>
+              <p>Learn how to develop Android and IOS mobile applications.</p>
+              <a href="http://157.230.37.96/maw/indexAndroid.php">
+                <button>Apply Now
+                  <span>></span>
+                </button>
+              </a>
+            </div>
+          </div><!--End Icon Box -->
+
+          <div class="col-xl-3 col-md-6" data-aos="fade-up" data-aos-delay="200">
+            <div class="icon-box">
+              <img src="assets/img/web3.png" id="img3">
+              <h4>Lets grow your Web development Career to great heights</h4>
+              <br>
+              <p>Acquire fullstack web development skills in HTML/CSS, Javascript, Angular, Java, PHP, NodeJs and Mysql.</p>
+              <a href="http://157.230.37.96/maw/indexWeb.php">
+                <button>Apply Now
+                  <span>></span>
+                </button>
+              </a>
+            </div>
+          </div><!--End Icon Box -->
+
+          <div class="col-xl-3 col-md-6" data-aos="fade-up" data-aos-delay="300">
+            <div class="icon-box">
+              <img src="assets/img/data.png" id="img3">
+              <h4>Learn how to build relational databases</h4>
+              <br>
+              <p>Get equiped with skills in designing and programming relational databases using
+                the structured query language (sql) and mysql database technology.</p>
+              <a href="http://157.230.37.96/maw/indexDatabase.php">
+                <button>Apply Now
+                  <span>></span>
+                </button>
+              </a>
+            </div>
+          </div><!--End Icon Box -->
+
+          <div class="col-xl-3 col-md-6" data-aos="fade-up" data-aos-delay="500">
+            <div class="icon-box">
+              <img src="assets/img/hy.png" id="img3">
+              <h4>Become  a hybrid mobile application developer</h4>
+              <br>
+              <p>Gain skills in hybrid mobile development using Google's flutter technology</p>
+              <a href="http://157.230.37.96/maw/indexAndroid.php">
+                <button>Apply Now
+                  <span>></span>
+                </button>
+              </a>
+            </div>
+          </div><!--End Icon Box -->
+
+        </div>
+      </div>
+    </div>
+
+    </div>
+  </section>
+  <!-- End Hero Section -->
+
+  <main id="main">
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer" class="footer">
+
+    <div class="container">
+      <div class="row gy-4">
+        <div class="col-lg-5 col-md-12 footer-info">
+          <a href="index.html" class="logo d-flex align-items-center">
+            <span>Maw Institute</span>
+          </a>
+          <p >Unlock your software development and software engineering potential at Maw institute.Here, we transform aspiring software engineers and software developers into industry-ready professionals</p>
+          <div class="social-links d-flex mt-4">
+            <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+            <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+            <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+            <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+          </div>
+        </div>
+
+        <div class="col-lg-2 col-6 footer-links">
+          <h4>Useful Links</h4>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="termsAndCondition.php">Terms of service</a></li>
+            <li><a href="privacy.php">Privacy policy</a></li>
+          </ul>
+        </div>
+
+        <div class="col-lg-2 col-6 footer-links">
+          <h4>Our Courses</h4>
+          <ul>
+            <li><a href="http://157.230.37.96/maw/indexShortCourses.php">Short Programming Courses</a></li>
+            <li><a href="http://157.230.37.96/maw/indexWeb.php">Web Development</a></li>
+            <li><a href="http://157.230.37.96/maw/indexAndroid.php">Mobile  App Development</a></li>
+            <li><a href="http://157.230.37.96/maw/indexAndroid.php">Hybrid App Development</a></li>
+            <li><a href="http://157.230.37.96/maw/indexDatabase.php">Database Programming</a></li>
+            
+          </ul>
+        </div>
+
+        <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
+          <h4>Contact Us</h4>
+          <p style="color:silver;">
+            Nairobi <br>
+            Utali house<br>
+            Kenya <br><br>
+            <strong>Phone: </strong> +254715744358,+254726586630<br>
+            <strong>Email: </strong>info@mawinstitute.com,<br>admission@mawinstitute.com,<br>sales@mawinstitute.com<br>
+          </p>
+
+        </div>
+
+      </div>
+    </div>
+
+    <div class="container mt-4">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Maw</span></strong>. All Rights Reserved
+      </div>
+     
+    </div>
+
+  </footer><!-- End Footer -->
+  <!-- End Footer -->
+
+  <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script>
+    let subMenu=document.getElementById("subMenu");
+    function toggleMenu(){
+      subMenu.classList.toggle("open-menu");
+      
+    }
+  </script>
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
